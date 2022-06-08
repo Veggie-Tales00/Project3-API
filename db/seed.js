b11db2f2cd4e34f03b6ebd10f0c02510ec042845
Mike Sabella <mikeysab@hotmail.om>
2022-06-08 13:50:03 -0700

    Updated both wineSeed and dishSeed

--- db/seed.js
+++ db/seed.js
@@ -4,16 +4,16 @@ const Dish = require('../models/Dishes')
 const wineData = require('./wineSeed.json')
 const dishData = require('./dishSeed.json')
 
-Wine.deleteMany({}).then(()=>{
-    Wine.create(wineData).then(wineList =>{
+Wine.deleteMany({}).then(() => {
+    Wine.create(wineData).then(wineList => {
         console.log(wineList)
-    }).then(()=>{
-        Dish.deleteMany({}).then(()=>{
-            Dish.create(dishData).then(dishList =>{
-                console.log(dishList)
-                process.exit()
-            })
-        })
+    }).then(() => {
+        // Dish.deleteMany({}).then(()=>{
+        //     Dish.create(dishData).then(dishList =>{
+        //         console.log(dishList)
+        //         process.exit()
+        //     })
+        // })
     })
 })
 
